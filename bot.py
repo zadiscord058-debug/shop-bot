686abc28948f4ef785b317baf6dfd3241a8ab8cb
zadiscord058-debug <zadiscord058@gmail.com>
2026-04-13 13:53:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.members = True  # IMPORTANT for role.members
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 FOUNDER_ROLE_ID = 1435194033558392842
 
