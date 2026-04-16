4e12eeb3a8df02549dabb3a51aa8d23051e4a883
zadiscord058-debug <zadiscord058@gmail.com>
2026-04-17 00:57:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ afk_users = {}
 
 VOUCH_FILE = "vouches.json"
 CONFIG_FILE = "config.json"
-PURPLE = discord.Colour.light_blue()
+PURPLE = discord.Colour.from_rgb(88, 166, 255)
 SERVER_NAME = "Foltyn's Family"
 LOG_CHANNEL_NAME = "mod-logs"
 COOLDOWN = 300
